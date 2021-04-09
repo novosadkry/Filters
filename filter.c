1ad79cb21a2f129d4a156582268e6684acea6520
Kryštof Novosad <novosadkry@seznam.cz>
2021-04-09 11:50:21 +0200

    Prepare the sobel operator filter

--- filter.c
+++ filter.c
@@ -1,6 +1,20 @@
 #include "filter.h"
 #include <stdlib.h>
 
+float sobel_kernel[2][9] =
+{
+    {
+         1,  0, -1,
+         2,  0, -2,
+         1,  0, -1
+    },
+    {
+         1,  2,  1,
+         0,  0,  0,
+        -1, -2, -1
+    }
+};
+
 void f_threshold_rgb(Pixel_RGB *in, Pixel_RGB *out, int w, int h, float value)
 {
     for (int x = 0; x < w; x++)
@@ -16,6 +30,17 @@ void f_threshold_rgb(Pixel_RGB *in, Pixel_RGB *out, int w, int h, float value)
                 out[i] = (Pixel_RGB) { 255, 255, 255 };
             else
                 out[i] = (Pixel_RGB) { 0, 0, 0 };
+        }
+    }
+}
+
+void f_sobel_rgb(Pixel_RGB *in, Pixel_RGB *out, int w, int h)
+{
+    for (int x = 0; x < w; x++)
+    {
+        for (int y = 0; y < h; y++)
+        {
+
         }
     }
 }
--- filter.h
+++ filter.h
@@ -4,5 +4,6 @@
 #include "image.h"
 
 void f_threshold_rgb(Pixel_RGB *in, Pixel_RGB *out, int w, int h, float value);
+void f_sobel_rgb(Pixel_RGB *in, Pixel_RGB *out, int w, int h);
 
 #endif
