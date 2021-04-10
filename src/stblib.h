5a0c26a55022ee49055bcc9ccd5e12bce8933206
Kryštof Novosad <novosadkry@seznam.cz>
2021-04-10 20:31:04 +0200

    Ingore warnings coming from stb headers

--- src/stblib.h
+++ src/stblib.h
@@ -1,7 +1,12 @@
 #ifndef STBLIB_H
 #define STBLIB_H
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+
 #include <stb_image.h>
 #include <stb_image_write.h>
+
+#pragma GCC diagnostic pop
 
 #endif
